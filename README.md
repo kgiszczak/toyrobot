68bfc5f5fb84be452a4f3802de0d0fa73e9c9be7
Kamil Giszczak <beerkg@gmail.com>
2015-09-08 10:05:51 +0200

    Add sample data and instructions to README file

---
+++ README.md
@@ -0,0 +1,14 @@
+# Toyrobot
+
+To run specs:
+```
+bundle install
+rspec
+```
+
+To run application with sample data:
+```
+ruby ./app.rb
+```
+
+This will load sample data from `test_data.txt` and run the robot against every line in this file.
--- app.rb
+++ app.rb
@@ -1,1 +1,12 @@
 require 'bundler/setup'
+
+lib = File.expand_path('lib', File.dirname(__FILE__))
+$:.unshift(lib) unless $:.include?(lib)
+
+require 'runner'
+
+File.read(File.expand_path('test_data.txt', File.dirname(__FILE__))).each_line do |line|
+  puts "Input: #{line}"
+  Runner.new(line).run
+  puts '------------'
+end
